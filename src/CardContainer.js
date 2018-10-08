e59a0fe84f12429a3cfbe1a2c597a6a60c6561e0
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-07 21:48:20 -0600

    Lint CardContainer

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import Card from './Card'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Card from './Card';
+import PropTypes from 'prop-types';
 
 
 const CardContainer = ( {data, compareCards} ) => {
   
-  let dataKeys = Object.keys(data)
+  let dataKeys = Object.keys(data);
   
-  let counter = 0
+  let counter = 0;
   
   const exp = dataKeys.map( point =>
-    <Card {...data[point]} className={'card' + [point]} key={counter++} compareCards={ compareCards } />
-  )
+    <Card {...data[point]} 
+      className={'card' + [point]} 
+      key={counter++} 
+      compareCards={ compareCards } 
+    />
+  );
   return (
-        <div className='card-container'>
-         { exp }
-        </div>
-  )
-}
+    <div className='card-container'>
+      { exp }
+    </div>
+  );
+};
 
 
 
 CardContainer.propTypes = {
   data: PropTypes.array,
   compareCards: PropTypes.func
-}
+};
 
-export default CardContainer
+export default CardContainer;
